f3f5919adefdde48ebe7c2bd8df4c88402fe580c
Alexandre Ferreira <breadmodz@gmail.com>
2025-01-13 21:31:53 -0300

    Added hook of useDepartaments
    Hook to fetch information from the departments database.

--- mobile/app/(home)/tags.tsx
+++ mobile/app/(home)/tags.tsx
@@ -1,75 +1,16 @@
 import { router } from "expo-router"
-import { useState, useEffect } from "react"
 import { Feather } from "@expo/vector-icons"
-import {
-  View,
-  Text,
-  Alert,
-  FlatList,
-  ScrollView,
-  ActivityIndicator,
-} from "react-native"
+import { View, Text, FlatList, ScrollView } from "react-native"
 
-import { api } from "@/src/lib/axios"
-import { today, weekday, date_month } from "@/src/lib/dayjs"
+import { weekday, date_month } from "@/src/lib/dayjs"
+import { useDepartament } from "@/src/lib/DepartamentContext"
 
 import { Header } from "@/src/components/Header"
 import { Departament } from "@/src/components/Departament"
 import { EnvironmentsDate } from "@/src/components/EnvironmentsDate"
 
-interface Weekday {
-  id: number
-  day: number
-  departamentId: number
-}
-
-interface Departament {
-  id: number
-  title: string
-  isActive: boolean
-  weekdays: Weekday[]
-}
-
 export default function Tags() {
-  const [loading, setLoading] = useState(true)
-  const [departaments, setDepartaments] = useState<Departament[]>([])
-
-  useEffect(() => {
-    async function fetchDepartaments() {
-      try {
-        const response = await api.get<Departament[]>("/departaments")
-        const updatedDepartaments = response.data.map((departament) => {
-          // Verifica se algum 'day' em weekdays corresponde ao dia atual
-          const isActive = departament.weekdays.some(
-            (weekday) => weekday.day === today
-          )
-
-          return {
-            ...departament,
-            isActive,
-          }
-        })
-
-        setDepartaments(updatedDepartaments)
-      } catch (error) {
-        Alert.alert("Erro!", "Erro ao buscar departamentos.", [
-          {
-            text: "Cancelar",
-            onPress: () => console.log("Cancelar pressionado!"),
-            style: "cancel",
-          },
-          {
-            text: "OK",
-            onPress: () => console.log("OK pressionado!"),
-          },
-        ])
-      } finally {
-        setLoading(false)
-      }
-    }
-
-    fetchDepartaments()
-  }, [])
+  const { departaments } = useDepartament()
 
   return (
     <View className="items-center bg-background flex-1">
@@ -79,35 +20,31 @@ export default function Tags() {
         className="flex-1 w-full max-w-[90%] pt-10"
       >
         <EnvironmentsDate date={date_month} weekday={weekday} />
-        {loading ? (
-          <ActivityIndicator size="small" color="#fff" />
-        ) : (
-          <FlatList
-            data={departaments}
-            keyExtractor={(item) => String(item.id)}
-            renderItem={({ item }) => (
-              <Departament
-                title={item.title}
-                active={item.isActive}
-                onPress={
-                  item.isActive
-                    ? () =>
-                        router.push({
-                          pathname: "/tags/[id]",
-                          params: { id: item.id, title: item.title },
-                        })
-                    : undefined
-                }
-              />
-            )}
-            showsVerticalScrollIndicator={false}
-            scrollEnabled={false}
-          />
-        )}
+        <FlatList
+          data={departaments}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => (
+            <Departament
+              title={item.title}
+              active={item.isActive}
+              onPress={
+                item.isActive
+                  ? () =>
+                      router.push({
+                        pathname: "/tags/[id]",
+                        params: { id: item.id, title: item.title },
+                      })
+                  : undefined
+              }
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          scrollEnabled={false}
+        />
         <View className="mt-10 border-dashed border rounded-lg border-zinc-500 p-6">
           <View className="flex-row space-x-4 items-center justify-center">
             <Feather name="alert-octagon" size={24} color="#F7DD43" />
-            <Text className="text-[#F7DD43] font-rajdhani_700 text-sm leading-snug">
+            <Text className="text-yelp font-rajdhani_700 text-sm leading-snug">
               Use as funções deste app com segurança. Tenha cuidado para evitar
               problemas maiores!
             </Text>
---
+++ mobile/src/lib/DepartamentContext.tsx
@@ -0,0 +1,83 @@
+import { ActivityIndicator, Alert } from "react-native"
+import { createContext, useContext, useEffect, useState } from "react"
+
+import { api } from "./axios"
+import { today } from "./dayjs"
+
+interface Weekday {
+  id: number
+  day: number
+  departamentId: number
+}
+
+interface Departament {
+  id: number
+  title: string
+  isActive: boolean
+  weekdays: Weekday[]
+}
+
+interface DepartamentContextType {
+  departaments: Departament[]
+}
+
+const DepartamentContext = createContext<DepartamentContextType | undefined>(
+  undefined
+)
+
+export const DepartamentProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [loading, setLoading] = useState(false)
+  const [departaments, setDepartaments] = useState<Departament[]>([])
+
+  useEffect(() => {
+    async function fetchDepartaments() {
+      try {
+        const response = await api.get<Departament[]>("/departaments")
+        const updatedDepartaments = response.data.map((departament) => {
+          const isActive = departament.weekdays.some(
+            (weekday) => weekday.day === today // Certifique-se de que 'today' está definido
+          )
+
+          return {
+            ...departament,
+            isActive,
+          }
+        })
+
+        setDepartaments(updatedDepartaments)
+      } catch (error) {
+        Alert.alert("Erro!", "Erro ao buscar departamentos.", [
+          {
+            text: "Cancelar",
+            onPress: () => console.log("Cancelar pressionado!"),
+            style: "cancel",
+          },
+          {
+            text: "Buscar novamente",
+            onPress: () => fetchDepartaments(),
+          },
+        ])
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchDepartaments()
+  }, [])
+
+  return (
+    <DepartamentContext.Provider value={{ departaments }}>
+      {children}
+    </DepartamentContext.Provider>
+  )
+}
+
+export const useDepartament = () => {
+  const context = useContext(DepartamentContext)
+  if (context === undefined) {
+    throw new Error("useDepartament must be used within a DepartamentProvider")
+  }
+  return context
+}
