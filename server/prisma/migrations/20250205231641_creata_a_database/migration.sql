9d059c4c088f7833e394c643172fe5d113dda26b
Alexandre Ferreira <breadmodz@gmail.com>
2025-02-05 20:30:35 -0300

    fix: database fix, added PostgreSQL

---
+++ server/prisma/migrations/20250205231641_creata_a_database/migration.sql
@@ -0,0 +1,51 @@
+-- CreateTable
+CREATE TABLE "departaments" (
+    "id" SERIAL NOT NULL,
+    "isActive" BOOLEAN NOT NULL,
+    "title" TEXT NOT NULL,
+
+    CONSTRAINT "departaments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "weekdays" (
+    "id" SERIAL NOT NULL,
+    "day" INTEGER NOT NULL,
+    "departamentId" INTEGER NOT NULL,
+
+    CONSTRAINT "weekdays_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "streets" (
+    "id" SERIAL NOT NULL,
+    "isActive" BOOLEAN NOT NULL,
+    "code" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "weekday" INTEGER NOT NULL,
+    "departamentId" INTEGER NOT NULL,
+
+    CONSTRAINT "streets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "licenses" (
+    "id" SERIAL NOT NULL,
+    "key" TEXT NOT NULL,
+    "createdAt" TEXT NOT NULL,
+    "expiresAt" TEXT NOT NULL,
+
+    CONSTRAINT "licenses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "departaments_id_key" ON "departaments"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "licenses_key_key" ON "licenses"("key");
+
+-- AddForeignKey
+ALTER TABLE "weekdays" ADD CONSTRAINT "weekdays_departamentId_fkey" FOREIGN KEY ("departamentId") REFERENCES "departaments"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "streets" ADD CONSTRAINT "streets_departamentId_fkey" FOREIGN KEY ("departamentId") REFERENCES "departaments"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- server/src/server.ts
+++ server/src/server.ts
@@ -8,5 +8,8 @@ app.register(cors)
 app.register(appRoutes)
 
 app
-  .listen({ port: 3333, host: "0.0.0.0" })
-  .then(() => console.log("HTTP Server running!"))
+  .listen({
+    host: "0.0.0.0",
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
+  })
+  .then(() => console.log("HTTP Server Running!"))
