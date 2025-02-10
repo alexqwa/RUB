62f904bec1ab9291b4a1093ccd1c499bc411a230
Alexandre Ferreira <breadmodz@gmail.com>
2025-02-09 23:27:12 -0300

    fix: added loading in verify license button and added section auth

--- mobile/app/(auth)/index.tsx
+++ mobile/app/(auth)/index.tsx
@@ -1,9 +1,63 @@
-import { View, Text } from "react-native"
+import {
+  View,
+  Text,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from "react-native"
+
+import { Header } from "@/src/components/Header"
+import { router } from "expo-router"
 
 export default function SignIn() {
   return (
-    <View>
-      <Text>Hello</Text>
+    <View className="bg-background flex-1 items-center">
+      <Header title="Autenticação" />
+      <KeyboardAvoidingView className="w-full flex-1">
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            marginTop: "auto",
+            marginBottom: "auto",
+            alignItems: "center",
+          }}
+        >
+          <View className="flex-1 items-center w-full max-w-[90%]">
+            <View className="mb-10 items-center">
+              <Text className="text-white text-4xl font-rajdhani_700">
+                FAZER CADASTRO
+              </Text>
+              <Text className="text-white text-center text-base font-rajdhani_700">
+                Basta preencher esses dados e você{"\n"}estará conosco.
+              </Text>
+            </View>
+            <View className="w-full space-y-3">
+              <TextInput
+                placeholder="DIGITE SEU NOME"
+                placeholderTextColor="#8D8D99"
+                selectionColor="#F7DD43"
+                className="bg-foreground text-white font-rajdhani_700 rounded-lg px-4 h-13 border border-outline"
+              />
+              <TextInput
+                placeholder="DIGITE SUA LICENÇA"
+                placeholderTextColor="#8D8D99"
+                selectionColor="#F7DD43"
+                className="bg-foreground text-white font-rajdhani_700 rounded-lg px-4 h-13 border border-outline"
+              />
+              <TouchableOpacity
+                activeOpacity={0.7}
+                onPress={() => router.push("/license")}
+                className="h-13 items-center justify-center bg-yelp rounded-lg"
+              >
+                <Text className="font-rajdhani_700 uppercase text-base">
+                  Concluir cadastro
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </View>
   )
 }
--- mobile/app/license.tsx
+++ mobile/app/license.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import clsx from "clsx"
+import { useEffect, useState } from "react"
 import {
   View,
   Text,
@@ -6,6 +7,7 @@ import {
   TextInput,
   ScrollView,
   TouchableOpacity,
+  ActivityIndicator,
   KeyboardAvoidingView,
 } from "react-native"
 
@@ -13,16 +15,34 @@ import { Header } from "@/src/components/Header"
 import { useLicense } from "@/src/lib/LicenseContext"
 
 export default function License() {
-  const [licenseKey, setLicenseKey] = useState("")
   const { verifyLicense } = useLicense()
+  const [loading, setLoading] = useState(false)
+  const [enabled, setEnabled] = useState(false)
+  const [licenseKey, setLicenseKey] = useState("")
 
   async function handleVerify() {
-    await verifyLicense(licenseKey)
+    setLoading(true)
+
+    try {
+      await verifyLicense(licenseKey)
+    } catch (error) {
+      console.log("Erro ao verificar a licença. Tente novamente.")
+    } finally {
+      setLoading(false)
+    }
   }
+
+  useEffect(() => {
+    if (licenseKey) {
+      setEnabled(false)
+    } else {
+      setEnabled(true)
+    }
+  }, [licenseKey])
 
   return (
     <View className="bg-background flex-1 items-center">
-      <Header back={false} title="Verificação de Licença" />
+      <Header title="Verificação de Licença" />
       <KeyboardAvoidingView
         className="flex-1 w-full"
         behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -52,16 +72,26 @@ export default function License() {
                 placeholder="DIGITE SUA LICENÇA"
                 placeholderTextColor="#8D8D99"
                 selectionColor="#F7DD43"
-                className="bg-foreground text-yelp font-rajdhani_700 rounded-lg px-4 h-13 border border-outline"
+                className="bg-foreground text-white font-rajdhani_700 rounded-lg px-4 h-13 border border-outline"
               />
               <TouchableOpacity
                 activeOpacity={0.7}
+                disabled={loading || enabled}
                 onPress={handleVerify}
-                className="bg-[#F7DD43] h-13 rounded-lg items-center justify-center"
+                className={clsx(
+                  "bg-yelp h-13 rounded-lg items-center justify-center transition-all",
+                  {
+                    ["bg-[#cab332]"]: enabled === true,
+                  }
+                )}
               >
-                <Text className="font-rajdhani_700 uppercase text-base">
-                  Fazer login
-                </Text>
+                {loading ? (
+                  <ActivityIndicator size="small" color="#202024" />
+                ) : (
+                  <Text className="font-rajdhani_700 uppercase text-base">
+                    Fazer login
+                  </Text>
+                )}
               </TouchableOpacity>
             </View>
           </View>
