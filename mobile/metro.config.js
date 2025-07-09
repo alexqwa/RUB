2376112cd5ba1e8af9853dd553ce72e7b57dfde0
Alexandre Ferreira <breadmodz@gmail.com>
2025-07-08 21:07:01 -0300

    Changelog v.1.7.5 (08/07/2025)
    - Corrigido problema de compilação
    - Ajustado problema de conexão com db

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,40 +1,40 @@
-// const { getDefaultConfig } = require('expo/metro-config');
-
-// const config = getDefaultConfig(__dirname)
-// const { transformer, resolver } = config;
-
-// const {
-//   wrapWithReanimatedMetroConfig,
-// } = require('react-native-reanimated/metro-config');
-
-// config.transformer = {
-//   ...transformer,
-//   babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
-// };
-// config.resolver = {
-//   ...resolver,
-//   assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-//   sourceExts: [...resolver.sourceExts, 'svg'],
-// };
-
-// module.exports = wrapWithReanimatedMetroConfig(config);
-
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = (() => {
-  const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
+const { transformer, resolver } = config;
 
-  const { transformer, resolver } = config;
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
 
-  config.transformer = {
-    ...transformer,
-    babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
-  };
-  config.resolver = {
-    ...resolver,
-    assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-    sourceExts: [...resolver.sourceExts, 'svg'],
-  };
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
+};
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...resolver.sourceExts, 'svg'],
+};
 
-  return config;
-})();
+module.exports = wrapWithReanimatedMetroConfig(config);
+
+// const { getDefaultConfig } = require('expo/metro-config');
+
+// module.exports = (() => {
+//   const config = getDefaultConfig(__dirname);
+
+//   const { transformer, resolver } = config;
+
+//   config.transformer = {
+//     ...transformer,
+//     babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
+//   };
+//   config.resolver = {
+//     ...resolver,
+//     assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+//     sourceExts: [...resolver.sourceExts, 'svg'],
+//   };
+
+//   return config;
+// })();
